244bef9b80ba6f74f63328280f4db648ee3a74ac
bart-roels <bart.roels@student.howest.be>
2023-01-09 18:22:33 +0100

    Feat: Added flask setup app.py

---
+++ Backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+# Start app
+app = Flask(__name__)
+CORS(app)
+
+# Custom endpoint
+endpoint = '/api/v1'
+
+
+@app.route(endpoint + '/test/', methods=['GET'])
+def test():
+    if request.method == 'GET':
+        return "Hello World!"
+
+# Start app
+if __name__ == '__main__':
+    app.run(debug=True)
