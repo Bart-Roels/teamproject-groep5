a486ae423fed64dac05560d1812464bdbb7ec928
Bart-Roels <bart.roels@student.howest.be>
2023-01-10 11:19:52 +0100

    Feat: memory game

--- Backend/Sender.py
+++ Backend/Sender.py
@@ -9,7 +9,6 @@ from paho.mqtt import client as mqtt_client
 broker = '192.168.168.166'
 port = 1883
 topic = "testTopic"
-# generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 username = 'paaltje'
 password = 'pole'
