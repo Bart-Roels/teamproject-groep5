1b5b7bbb11f913642eb7a41b4ce18ab0ec781aad
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-01-12 13:08:38 +0100

    Feat: Template with mqtt

--- Testing/app.py
+++ Testing/app.py
@@ -1,46 +1,61 @@
+import time
 from flask import Flask, request
 import paho.mqtt.client as mqtt
 import socket
 
 app = Flask(__name__)
 
-
-# Define the callbacks for connecting, publishing and receiving messages
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("1")
+# GLOBAL VARIABLES
+sequence = []
 
 
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
 
-# Create the MQTT client and set the callbacks
+def memory_game():
+    global sequence
+    print("start memory game")
+    # generate random sequence
+    
+
+
+
+# FLASK route to start the game
+
+
+# MQQT CLIENT
+def on_message(client, userdata, message):
+    # print topic and message
+    topic = message.topic
+    message = message.payload.decode("utf-8")
+    print(f"Topic: {topic}, Message: {message}")
+    if topic == "game":
+        if message == "memory":
+            memory_game()
+        elif message == "redblue":
+            print("redblue")
+        elif message == "zen":
+            print("zen")
+        elif message == "minesweepr":
+            print("minesweepr")
+        
+    
+
 client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-# Connect to the MQTT WebSocket
-client.connect("ws://localhost", port=9100, keepalive=60)
-
-# Start the loop to process incoming MQTT messages
+client.connect("127.0.0.1", 1883)
+client.on_message= on_message 
+# Hier zet je MQQT CODE VOOR NAAR WEB SERVER TE STUREN 
+# Subscribe to the topic "game"
+client.subscribe("games")
+client.subscribe("buttons")
 client.loop_forever()
 
 
-# FLASK
-
-@app.route('/turn_on_led', methods=['POST'])
-def turn_on_led():
-    # Get the LED ID from the request
-    data = request.get_json()
-    led_id = data.get('led_id')
-
-    # Publish the message to the "leds/1" topic
-    client.publish("leds/{}".format(led_id), 'on')
-
-    return "Led turned on!"
-
+# turn on all led's mqqt
+def on_all():
+    for i in range(1, 5):
+        client.publish(str(i), "on")
+        time.sleep(1)
+        client.publish(str(i), "off")
 
 if __name__ == '__main__':
+    print("Starting server")
     app.run(debug=True)
--- Testing/testing.js
+++ Testing/testing.js
@@ -2,44 +2,66 @@
 const client = mqtt.connect('ws://localhost:9001');
 
 client.on('connect', () => {
-    console.log('Connected to the MQTT WebSocket');
-    // Subscribe to the topics
-    client.subscribe('1');
-    client.subscribe('2');
-    client.subscribe('3');
-    client.subscribe('4');
-    // Handle messages received on the subscribed topic
-    client.on('message', (topic, message) => {
-        message = message.toString();
-        console.log('Received message on topic ' + topic + ': ' + message);
-        // Check the message
-        if (message === 'on') {
-            if (topic === '1') {
-                console.log('LED 1 on');
-                // Set opacity to 1
-                document.getElementById('led-1').setAttribute('opacity', '1');
-            } else if (topic === '2') {
-                document.getElementById('led-2').setAttribute('opacity', '1');
-            } else if (topic === '3') {
-                document.getElementById('led-3').setAttribute('opacity', '1');
-            } else if (topic === '4') {
-                document.getElementById('led-4').setAttribute('opacity', '1');
-            }
-        } else if (message === 'off') {
-            if (topic === '1') {
-                document.getElementById('led-1').setAttribute('opacity', '0.3');
-            } else if (topic === '2') {
-                document.getElementById('led-2').setAttribute('opacity', '0.3');
-            } else if (topic === '3') {
-                document.getElementById('led-3').setAttribute('opacity', '0.3');
-            } else if (topic === '4') {
-                document.getElementById('led-4').setAttribute('fill', 'white');
-            }
-        }
-    });
+  console.log('Connected to the MQTT WebSocket');
+  // Subscribe to the topics
+  client.subscribe('1');
+  client.subscribe('2');
+  client.subscribe('3');
+  client.subscribe('4');
+  // Handle messages received on the subscribed topic
+  client.on('message', (topic, message) => {
+    message = message.toString();
+    console.log('Received message on topic ' + topic + ': ' + message);
+    // Check the message
+    if (message === 'on') {
+      if (topic === '1') {
+        console.log('LED 1 on');
+        // Set opacity to 1
+        document.getElementById('led-1').setAttribute('opacity', '1');
+      } else if (topic === '2') {
+        document.getElementById('led-2').setAttribute('opacity', '1');
+      } else if (topic === '3') {
+        document.getElementById('led-3').setAttribute('opacity', '1');
+      } else if (topic === '4') {
+        document.getElementById('led-4').setAttribute('opacity', '1');
+      }
+    } else if (message === 'off') {
+      if (topic === '1') {
+        document.getElementById('led-1').setAttribute('opacity', '0.3');
+      } else if (topic === '2') {
+        document.getElementById('led-2').setAttribute('opacity', '0.3');
+      } else if (topic === '3') {
+        document.getElementById('led-3').setAttribute('opacity', '0.3');
+      } else if (topic === '4') {
+        document.getElementById('led-4').setAttribute('opacity', '0.3');
+      }
+    }
+  });
 });
+
+// Listen to the buttons
+const listen = () => {
+  const btnsGame = document.querySelectorAll('.js-button-game');
+  btnsGame.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const topic = btn.getAttribute('data-game-id');
+      client.publish('games', topic);
+      // console.log(`Start game ${topic}`);
+    });
+  });
+
+  const btnsLed = document.querySelectorAll('.js-button-led');
+  btnsLed.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const message = btn.getAttribute('data-led-id');
+      client.publish("buttons", message);
+      // console.log(`Button ${message} pressed`);
+    });
+  });
+};
 
 // Dom loaded
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM loaded');
+  console.log('DOM loaded');
+  listen();
 });
