1df4ed64a2ef4808d1dcb4a89b42a8199c7b0142
Bart-Roels <bart.roels@student.howest.be>
2023-01-13 10:18:59 +0100

    feat: aanpassing memory game

--- Testing/app.py
+++ Testing/app.py
@@ -11,23 +11,10 @@ game = "none"
 counter = 0 # for memory
 sequence_number = 1 # for memory
 sequence = [] # for memory
+won = [2,2,2,2] # for memory 
+lose = [0,0,0,0]
 
-# Simple send message function
-def send_messagetest():
-    sequence = [1, 2, 3, 4]
-    while sequence != []:
-        client.loop_start()
-        # Publish message
-        client.publish(str(sequence[0]), str(sequence[0]))
-        print(f"Sending: {sequence[0]}")
 
-        time.sleep(2)
-
-        print("Sending: off")
-        client.publish(str(sequence[0]), "off")
-
-        sequence.pop(0)
-        client.loop_stop()
 
 def send_message():
     client.loop_start()
@@ -74,25 +61,38 @@ def on_message(client, userdata, message):
 
 # GAMES
 def generate_sequence(sequence_number):
-    leds = [1, 2, 3, 4]
-    sequence = []
+    leds = [0, 1, 2, 3]
+    global sequence
     # Generate sequence
     for i in range(sequence_number):
         led = leds[random.randint(0, len(leds)-1)]
         sequence.append(led)
     return sequence
 
-def send_sequence(sequence):
-    while sequence != []:
-        client.loop_start()
-        # Publish message
-        client.publish(str(sequence[0]), str(sequence[0]))
-        print(f"Sending: {sequence[0]}")
+
+def send_sequence(sequence, blink = False):
+    if(blink == True):
+        for item in sequence:
+            # Publish message
+            client.publish(str(item), str(item))
+            print(f"Sending: {item}")
+
+            client.loop()
+            time.sleep(2)
+
+            print("Sending: off")
+            client.publish(str(item), "off")
+    else:
+        client.publish(str(0), str(sequence[0]))
+        client.publish(str(1), str(sequence[1]))
+        client.publish(str(2), str(sequence[2]))
+        client.publish(str(3), str(sequence[3]))
+        client.loop()
         time.sleep(2)
-        print("Sending: off")
-        client.publish(str(sequence[0]), "off")
-        sequence.pop(0)
-        client.loop_stop()
+        for i in range(0,5):
+            client.publish(str(i), "off")
+
+
 
 
 
@@ -100,28 +100,37 @@ def check_sequence(received_sequence):
     global sequence_number
     global sequence
     global counter
+    global won
+    global lose
+    print(int(received_sequence) == sequence[counter])
     # check if the received sequence matches the current sequence
     if int(received_sequence) == sequence[counter]:
         counter += 1
         # check if the entire sequence has been matched
-        if counter == len(sequence):
-            print(f"You have won! your points: {sequence_number}")
+        if counter == len(sequence): 
+            # Resent counter en sequence 
             counter = 0
+            sequence = []
+            # You have won
+            print(f"You have won! your points: {sequence_number}")
+            client.publish("memorypoints", str(sequence_number))
+            send_sequence(won, False)
+            # Start new game en higher sequence 
             sequence_number += 1
-            # Start new sequence
             start_memory()
     else:
         print("Sequence does not match. Game over.")
-        # reset sequence number to 0
-        sequence_number = 0
         counter = 0
+        send_sequence(lose, False)
+        send_sequence(sequence=sequence, blink=True)
+
 
 
 def start_memory():
     global sequence_number
     global sequence
     sequence = generate_sequence(sequence_number)
-    send_sequence(sequence)
+    send_sequence(sequence, True)
 
 # MQQT CLIENT       
 client = mqtt.Client()
