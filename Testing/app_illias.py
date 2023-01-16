e112827703e816af2d81dfd27a12bce647b11770
PattynRuben <ruben.pattyn@student.howest.be>
2023-01-16 13:48:15 +0100

    feat: start minesweeper

--- Testing/app_illias.py
+++ Testing/app_illias.py
@@ -1,4 +1,6 @@
-import time, random
+import time
+import threading
+import random
 from flask import Flask, request
 import paho.mqtt.client as mqtt
 import socket
@@ -12,15 +14,12 @@ button2 = "off"
 button3 = "off"
 button4 = "off"
 
-#global variables for minesweeper
+random_led_zen = -1
+random_color_zen = -1
 
 list_minesweeper = []
-index_minesweeper = 0
-score_minesweeper = 0
 
 # turn on all led's mqqt
-
-
 def on_all():
     for i in range(1, 5):
         client.publish(str(i), "1")
@@ -28,48 +27,30 @@ def on_all():
         client.publish(str(i), "off")
 
 # Minesweeper
-
-
 def start_minesweepr():
-    print('start minesweeper')
     global list_minesweeper
-    global index_minesweeper
-    for i in range(4):
-        client.publish(str(i), "off")
-    list_minesweeper = random.sample(range(4),4)
-    show_sequence_minesweeper(1)
-    print(list_minesweeper)
+    global random_led_zen
+    if random_led_zen not in list_minesweeper:
+        random_led_zen = random.randint(1, 4)
+        list_minesweeper.append(random_led_zen)
+        client.publish(str(random_led_zen), str(random_led_zen))
+        print(list_minesweeper)
+    else:
+        random_led_zen = random.randint(1, 4)
 
 def analyse_buttons_minesweeper(number):
     global game
+    global random_led_zen
     global list_minesweeper
-    global index_minesweeper
-    global score_minesweeper
     print(f"button pressed: {number}; {game}")
-    if number == list_minesweeper[index_minesweeper]:
-        print('correct')
-        index_minesweeper += 1
-        if index_minesweeper == 4:
-            print('game won')
-            score_minesweeper += 1
-
-            index_minesweeper = 0
-            start_minesweepr()
-        show_sequence_minesweeper(index_minesweeper + 1)
-    else:
-        print('game over')
-        index_minesweeper = 0
+    if number == random_led_zen and random_led_zen not in list_minesweeper:
         start_minesweepr()
-def show_sequence_minesweeper(number):
-    print('show sequence')
-    global list_minesweeper
-    global index_minesweeper
-    for i in range(number):
-        client.publish(str(list_minesweeper[i]), str(list_minesweeper[i]))
-        
+    else:
+        print("game over")
+
+
+
 # MQQT CLIENT
-
-
 def on_message(client, userdata, message):
     global game
     # print topic and message
@@ -105,11 +86,11 @@ def on_message(client, userdata, message):
             print("minesweeper button incomming")
             analyse_buttons_minesweeper(int(message))
 
-
+        
 client = mqtt.Client()
 client.connect("127.0.0.1", 1883)
-client.on_message = on_message
-# Hier zet je MQQT CODE VOOR NAAR WEB SERVER TE STUREN
+client.on_message= on_message 
+# Hier zet je MQQT CODE VOOR NAAR WEB SERVER TE STUREN 
 # Subscribe to the topic "game"
 client.subscribe("games")
 client.subscribe("buttons")
