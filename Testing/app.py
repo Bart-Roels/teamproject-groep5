758e897375d765c69ff9615551199d15e130b2ef
PattynRuben <ruben.pattyn@student.howest.be>
2023-01-16 11:43:03 +0100

    feat: begin zen game

--- Testing/app.py
+++ Testing/app.py
@@ -1,4 +1,5 @@
 import time
+import random
 from flask import Flask, request
 import paho.mqtt.client as mqtt
 import socket
@@ -12,12 +13,21 @@ button2 = "off"
 button3 = "off"
 button4 = "off"
 
+# global variables for zen
+random_led_zen = -1
+random_color_zen = -1
+previous_time = -1
+total_score_zen = 0
+
 # turn on all led's mqqt
+
+
 def on_all():
     for i in range(1, 5):
         client.publish(str(i), "1")
         time.sleep(1)
         client.publish(str(i), "off")
+
 
 def zen_game():
     client.publish("1", "1")
@@ -37,6 +47,7 @@ def on_message(client, userdata, message):
         elif message == "redblue":
             game = "redblue"
             print("redblue")
+
         elif message == "zen":
             game = "zen"
             print("zen")
@@ -45,39 +56,79 @@ def on_message(client, userdata, message):
             game = "minesweepr"
             print("minesweepr")
     if topic == "buttons":
-        if message == "1":
-            button1 = "on"
-        elif message == "2":
-            button2 = "on"
-        elif message == "3":
-            button3 = "on"
-        elif message == "4":
-            button4 = "on"
+        if game == "memory":
+            # Do read button stuff voor memory
+            print("memory button incomming")
+        elif game == "redblue":
+            # Do read button stuff voor redblue
+            print("red vs blue button incomming")
 
-        # if game == "memory":
-        #     # Do read button stuff voor memory
-        #     print("memory button incomming")
-        # elif game == "redblue":
-        #     # Do read button stuff voor redblue
-        #     print("red vs blue button incomming")
-        # elif game == "zen":
-        #     # Do read button stuff voor zen
-        #     print("zen button incomming")
-        # elif game == "minesweepr":
-        #     # Do read button stuff voor minesweepr
-        #     print("minesweeper button incomming")
+        elif game == "zen":
+            # Do read button stuff voor zen
+            print("zen button incomming")
+            analyse_buttons_zen(int(message))
+        elif game == "minesweepr":
+            # Do read button stuff voor minesweepr
+            print("minesweeper button incomming")
 
-        
+
+def reward_response_time(response_time):
+    global total_score_zen
+    print(f"response time: {response_time}")
+    if response_time < 1:
+        print("reward 10")
+        total_score_zen += 10
+    elif response_time < 3:
+        print("reward 5")
+        total_score_zen += 5
+    elif response_time < 5:
+        print("reward 3")
+        total_score_zen += 3
+    elif response_time < 10:
+        print("reward 2")
+        total_score_zen += 2
+    else:
+        print("reward 1")
+        total_score_zen += 1
+
+    client.publish(
+        "memorypoints", f"score zen:{total_score_zen} - response time:{response_time}")
+
+
+def analyse_buttons_zen(number):
+    global game
+    global random_led_zen
+    global previous_time
+    print(f"button pressed: {number}; {game}")
+    if number == random_led_zen:
+        response_time = time.time() - previous_time
+        print(f"correct {response_time}")
+        reward_response_time(response_time)
+        zen_game()
+
+
+def zen_game():
+    global random_led_zen
+    global random_color_zen
+    global previous_time
+    print('zen game started')
+    for i in range(0, 4):
+        client.publish(str(i), "off")
+    random_led_zen = random.randint(0, 3)
+    random_color = random.randint(0, 3)
+    client.publish(str(random_led_zen), str(random_color))
+    previous_time = time.time()
+
+
 client = mqtt.Client()
 client.connect("127.0.0.1", 1883)
-client.on_message= on_message 
-# Hier zet je MQQT CODE VOOR NAAR WEB SERVER TE STUREN 
+client.on_message = on_message
+# Hier zet je MQQT CODE VOOR NAAR WEB SERVER TE STUREN
 # Subscribe to the topic "game"
 client.subscribe("games")
 client.subscribe("buttons")
-# client.loop_forever()
+client.loop_forever()
 
 if __name__ == '__main__':
     print("Starting server")
-    on_all()
     app.run(debug=False)
