f506f83dae1f1341ca0c4a70a58b99912fb4881d
PattynRuben <ruben.pattyn@student.howest.be>
2023-01-18 13:16:11 +0100

    feat: exception handing minesweeper

--- Testing_Zen_Game/app.py
+++ Testing_Zen_Game/app.py
@@ -1,12 +1,29 @@
 import time
 import threading
 import random
+import logging
 from flask import Flask, request
 import paho.mqtt.client as mqtt
 import socket
 
 app = Flask(__name__)
 
+# code for logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+# create a file handler
+handler = logging.FileHandler("app.log")
+handler.setLevel(logging.ERROR)
+
+# create a logging format
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
 # GLOBAL VARIABLES
 game = "none"
 button1 = "off"
@@ -42,70 +59,73 @@ def on_connect(client, userdata, flags, rc):  # Handels connection
 
 
 def on_disconnect(client, userdata, rc):
-    if rc != 0:
-        print("Unexpected MQTT disconnection. Attempting to reconnect.")
-        try:
+    try:
+        if rc != 0:
+            logger.error("Unexpected MQTT disconnection.")
             client.reconnect()
-        except socket.error:
-            print("Failed to reconnect. Exiting.")
+    except socket.error as e:
+        logger.error(f"Failed to reconnect: {e}. Exiting.")
 
 
 def on_message(client, userdata, message):
-    global game
-    global start_minesweeper
-    global new_game_minesweeper
-    global level_minesweeper
-    global score_minesweeper
-    # print topic and message
-    topic = message.topic
-    message = message.payload.decode("utf-8")
-    print(f"Topic: {topic}, Message: {message}")
-    if topic == "games":
-        if message == "memory":
-            game = "memory"
-            print("memory")
-        elif message == "redblue":
-            game = "redblue"
-            print("redblue")
-        elif message == "zen":
-            game = "zen"
-            print("zen")
-        elif message == "minesweepr":
-            game = "minesweepr"
-            print("minesweepr")
-            print('1: easy level, 2: medium level, 3: hard level')
-            level_minesweeper = int(input('choose level: '))
-            print(f'chosen level {level_minesweeper}')
-            score_minesweeper = 0
-            start_minesweeper = True
-            new_game_minesweeper = True
-    if topic == "buttons":
-        if game == "memory":
-            # Do read button stuff voor memory
-            print("memory button incomming")
-        elif game == "redblue":
-            # Do read button stuff voor redblue
-            print("red vs blue button incomming")
-        elif game == "zen":
-            # Do read button stuff voor zen
-            print("zen button incomming")
-        elif game == "minesweepr":
-            # Do read button stuff voor minesweepr
-            print("minesweeper button incomming")
-            
-            analyse_buttons_minesweeper(message)
-    if topic == "stop":
-        if game == "memory":
-            print("stop memory")
-        elif game == "redblue":
-            print("stop redblue")
-        elif game == "zen":
-            print("stop zen")
-        elif game == "minesweepr":
-            start_minesweeper = False
-            new_game_minesweeper = False
+    try:
+        global game
+        global start_minesweeper
+        global new_game_minesweeper
+        global level_minesweeper
+        global score_minesweeper
+        # print topic and message
+        topic = message.topic
+        message = message.payload.decode("utf-8")
+        print(f"Topic: {topic}, Message: {message}")
+        if topic == "games":
+            if message == "memory":
+                game = "memory"
+                print("memory")
+            elif message == "redblue":
+                game = "redblue"
+                print("redblue")
+            elif message == "zen":
+                game = "zen"
+                print("zen")
+            elif message == "minesweepr":
+                game = "minesweepr"
+                print("minesweepr")
+                print('1: easy level, 2: medium level, 3: hard level')
+                level_minesweeper = int(input('choose level: '))
+                print(f'chosen level {level_minesweeper}')
+                score_minesweeper = 0
+                start_minesweeper = True
+                new_game_minesweeper = True
+        if topic == "buttons":
+            if game == "memory":
+                # Do read button stuff voor memory
+                print("memory button incomming")
+            elif game == "redblue":
+                # Do read button stuff voor redblue
+                print("red vs blue button incomming")
+            elif game == "zen":
+                # Do read button stuff voor zen
+                print("zen button incomming")
+            elif game == "minesweepr":
+                # Do read button stuff voor minesweepr
+                print("minesweeper button incomming")
 
-            print("stop minesweepr")
+                analyse_buttons_minesweeper(message)
+        if topic == "stop":
+            if game == "memory":
+                print("stop memory")
+            elif game == "redblue":
+                print("stop redblue")
+            elif game == "zen":
+                print("stop zen")
+            elif game == "minesweepr":
+                start_minesweeper = False
+                new_game_minesweeper = False
+
+                print("stop minesweepr")
+    except Exception as e:
+        logger.error(e)
 # Minesweeper
 
 
@@ -116,57 +136,71 @@ def analyse_buttons_minesweeper(message):
     global haswon
     global haslost
     global busy_minesweeper
-    if not busy_minesweeper:
-        print(f'button {message} pressed; game: {game}')
-        print(f'controle{message == str(list_minesweeper[index_minesweeper])}')
-        if message == str(list_minesweeper[index_minesweeper]):
-            print('correct')
-            client.publish(str(list_minesweeper[index_minesweeper]), "2")
-            index_minesweeper += 1
-            print(f'score: {index_minesweeper}')
-            if index_minesweeper == 4:
-                print('game won')
-                score_minesweeper += 1
-                client.publish('memorypoints', str(score_minesweeper))
-                haswon = True
-        else:
-            print('wrong')
-            if level_minesweeper == 2 or level_minesweeper == 3:
-                print('game lost')
-                haslost = True
+    try:
+        if not busy_minesweeper:
+            print(f'button {message} pressed; game: {game}')
+            print(
+                f'controle{message == str(list_minesweeper[index_minesweeper])}')
+            if message == str(list_minesweeper[index_minesweeper]):
+                print('correct')
+                client.publish(str(list_minesweeper[index_minesweeper]), "2")
+                index_minesweeper += 1
+                print(f'score: {index_minesweeper}')
+                if index_minesweeper == 4:
+                    print('game won')
+                    score_minesweeper += 1
+                    client.publish('memorypoints', str(score_minesweeper))
+                    haswon = True
+            else:
+                print('wrong')
+                if level_minesweeper == 2 or level_minesweeper == 3:
+                    print('game lost')
+                    haslost = True
+    except Exception as e:
+        logger.error(e)
+
 
 def send_hint_function():
     global list_minesweeper
     global index_minesweeper
     global level_minesweeper
     global busy_minesweeper
-    busy_minesweeper = True
-    client.publish(str(list_minesweeper[0]), "1")
-    time.sleep(1)
-    client.publish(str(list_minesweeper[0]), "off")
-    busy_minesweeper = False
+    try:
+        busy_minesweeper = True
+        client.publish(str(list_minesweeper[0]), "1")
+        time.sleep(1)
+        client.publish(str(list_minesweeper[0]), "off")
+        busy_minesweeper = False
+    except Exception as e:
+        logger.error(e)
 
 
 def sequence_off():
     global busy_minesweeper
-    busy_minesweeper = True
-    print('sequence off')
-    for i in range(4):
-        client.publish(str(i), "off")
-    time.sleep(1)
-    busy_minesweeper = False
+    try:
+        busy_minesweeper = True
+        print('sequence off')
+        for i in range(4):
+            client.publish(str(i), "off")
+        time.sleep(1)
+        busy_minesweeper = False
+    except Exception as e:
+        logger.error(e)
 
 
 def sequence_mistake():
     global busy_minesweeper
-    busy_minesweeper = True
-    print('mistake')
-    for i in range(4):
-        client.publish(str(i), "0")
-    time.sleep(1)
-    for i in range(4):
-        client.publish(str(i), "off")
-    busy_minesweeper = False
+    try:
+        busy_minesweeper = True
+        print('mistake')
+        for i in range(4):
+            client.publish(str(i), "0")
+        time.sleep(1)
+        for i in range(4):
+            client.publish(str(i), "off")
+        busy_minesweeper = False
+    except Exception as e:
+        logger.error(e)
 
 
 def minesweeper():
@@ -204,8 +238,11 @@ def minesweeper():
                             send_hint_function()
                         index_minesweeper = 0
                         haslost = False
+    except Exception as e:
+        logger.error(e)
     finally:
         semaphore.release()
+
 
 # MQTT client
 client = mqtt.Client()
@@ -215,17 +252,23 @@ client.on_disconnect = on_disconnect
 
 
 def subscribing():
-    client.on_message = on_message
-    client.loop_forever()
+    try:
+        client.on_message = on_message
+        client.loop_forever()
+    except Exception as e:
+        logger.error(e)
 
 
 def start_threads():
-    # Start subscribing thread
-    print("Starting subscribing thread")
-    sub = threading.Thread(target=subscribing)
-    sub.start()
-    mine = threading.Thread(target=minesweeper)
-    mine.start()
+    try:
+        # Start subscribing thread
+        print("Starting subscribing thread")
+        sub = threading.Thread(target=subscribing)
+        sub.start()
+        mine = threading.Thread(target=minesweeper)
+        mine.start()
+    except Exception as e:
+        logger.error(e)
 
 
 # APP START
