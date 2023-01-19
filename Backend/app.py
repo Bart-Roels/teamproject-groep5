0f1850c5f3add8fa0ea21f032e27aa9d828e93bb
PattynRuben <ruben.pattyn@student.howest.be>
2023-01-19 13:46:56 +0100

    feat: code efficienter maken

--- Backend/app.py
+++ Backend/app.py
@@ -114,32 +114,21 @@ def on_disconnect(client, userdata, rc):  # Handels disconnection
 def on_message(client, userdata, message):  # Handels incomming messages
     try:
         # global variables
-        global game
-        global pauze_state
+        global game, pauze_state
 
         # Global variables memory
 
-        global start_memory_var
-        global new_game
-        global sequence_number
-        global sequence
+        global start_memory_var, new_game, sequence_number, sequence
 
         # Global variables redblue
-        global start_redvsblue_game
-        global new_game_redvsblue
-        global score_team_blue
-        global score_team_red
+        global start_redvsblue_game, new_game_redvsblue, score_team_blue, score_team_red
 
         # Global variables zen
-        global new_zen_game
-        global start_zen_game
-        global total_score_zen
+        global new_zen_game, start_zen_game, total_score_zen
 
-        # Global variables minesweepr
-        global start_minesweeper
-        global new_game_minesweeper
-        global level_minesweeper
-        global score_minesweeper
+
+        # Global variables minesweepr      
+        global start_minesweeper, new_game_minesweeper, level_minesweeper, score_minesweeper
 
         # print topic and message
         topic = message.topic
@@ -241,64 +230,43 @@ def handle_games():
         # global variables
         global pauze_state
         # Global variables memory
-        global start_memory_var
-        global sequence_number
-        global new_game
-        global haswon
-        global haslost
-        won = [2, 2, 2, 2]
-        lose = [0, 0, 0, 0]
+        global start_memory_var, sequence_number, new_game, haswon, haslost
 
         # Global variables redblue
-        global red_led, blue_led
-        global start_redvsblue_game
-        global new_game_redvsblue
+        global start_redvsblue_game, new_game_redvsblue
 
         # Global variables zen
-        global random_led_zen
-        global random_color_zen
-        global previous_time
-        global start_zen_game
-        global new_zen_game
+        global random_led_zen, random_color_zen, previous_time, start_zen_game, new_zen_game
 
         # Global variables minesweepr
-        global list_minesweeper
-        global index_minesweeper
-        global new_game_minesweeper
-        global start_minesweeper
-        global haswon
-        global haslost
-        global level_minesweeper
+        global list_minesweeper, index_minesweeper, new_game_minesweeper, start_minesweeper, haswon, haslost, level_minesweeper
 
         # Start memory
 
         while True:
             if not pauze_state:
-                if (start_memory_var == True):
-                    if (new_game == True):
+                if start_memory_var:
+                    if new_game:
                         sequence = generate_sequence(sequence_number)
                         send_sequence(sequence, True)
                         new_game = False
-                    else:
-                        if (haswon == True):
-                            # Play win sequence
-                            send_sequence(won)
-                            haswon = False
-                            # Start new sequence
-                            new_game = True
-                        elif (haslost == True):
-                            send_sequence(lose, False)
-                            # Replay sequence
-                            send_sequence(sequence, True)
-                            haslost = False
-                            print("You have lost replaying sequence")
-                if (start_redvsblue_game):
-                    if (new_game_redvsblue):
-                        print('new game')
-                        for i in range(0, 4):
+                    elif haswon:
+                        # Play win sequence
+                        send_sequence([2, 2, 2, 2])
+                        haswon = False
+                        # Start new sequence
+                        new_game = True
+                    elif (haslost == True):
+                        send_sequence([0, 0, 0, 0], False)
+                        # Replay sequence
+                        send_sequence(sequence, True)
+                        haslost = False
+                        print("You have lost replaying sequence")
+                if start_redvsblue_game:
+                    if new_game_redvsblue:
+                        for i in range(4):
                             client.publish(str(i), "off")
                         list_leds = random_leds()
-                        print(list_leds)
                         red_led = list_leds[0]
                         blue_led = list_leds[1]
                         client.publish(str(red_led), "0")
@@ -306,7 +274,7 @@ def handle_games():
                         new_game_redvsblue = False
                 if (start_zen_game):
                     if (new_zen_game):
-                        for i in range(0, 4):
+                        for i in range(4):
                             client.publish(str(i), "off")
                         random_led_zen = random.randint(0, 3)
                         random_color_zen = random.randint(0, 3)
@@ -316,29 +284,28 @@ def handle_games():
                                        str(random_color_zen))
                         previous_time = time.time()
                         new_zen_game = False
-                if (start_minesweeper):
-                    if (new_game_minesweeper):
+                if start_minesweeper:
+                    if new_game_minesweeper:
                         list_minesweeper = random.sample(range(4), 4)
                         print(f'list: {list_minesweeper}')
                         index_minesweeper = 0
                         print(f'level minesweeper: {level_minesweeper}')
-                        if level_minesweeper == 1 or level_minesweeper == 2:
+                        if level_minesweeper in (1, 2):
                             send_hint_function()
 
                         new_game_minesweeper = False
-                    else:
-                        if (haswon):
-                            print('Game has been won')
-                            sequence_off()
-                            haswon = False
-                            new_game_minesweeper = True
-                        elif (haslost):
-                            print('Game has been lost')
-                            sequence_mistake()
-                            if level_minesweeper == 2:
-                                send_hint_function()
-                            index_minesweeper = 0
-                            haslost = False
+                    elif (haswon):
+                        print('Game has been won')
+                        sequence_off()
+                        haswon = False
+                        new_game_minesweeper = True
+                    elif (haslost):
+                        print('Game has been lost')
+                        sequence_mistake()
+                        if level_minesweeper == 2:
+                            send_hint_function()
+                        index_minesweeper = 0
+                        haslost = False
     except Exception as e:
         print(e)  # to print the error
         logger.error(e)
@@ -350,12 +317,8 @@ def handle_games():
 
 def generate_sequence(sequence_number):
     try:
-        leds = [0, 1, 2]
         global sequence
-        # Generate sequence
-        for i in range(sequence_number):
-            led = leds[random.randint(0, len(leds)-1)]
-            sequence.append(led)
+        sequence = random.sample([0, 1, 2], sequence_number)
         return sequence
     except Exception as e:
         print(e)
@@ -436,25 +399,15 @@ def check_sequence(received_sequence):  # Check sequence
 
 def analyse_pressed_buttons_redvsblue(number):
     try:
-        global game
-        global start_redvsblue_game
         global new_game_redvsblue
-        print(f"red: {red_led} blue:{blue_led}")
-        print(f"button pressed: {number}; {game}")
         if number == red_led:
-            print("red wins")
             global score_team_red
             score_team_red += 1
-            print(f"score red: {score_team_red}")
             new_game_redvsblue = True
-
         elif number == blue_led:
-            print("blue wins")
             global score_team_blue
             score_team_blue += 1
-            print(f"score blue: {score_team_blue}")
             new_game_redvsblue = True
-
         client.publish(
             "memorypoints", f"score red:{score_team_red} score blue:{score_team_blue}")
     except socket.error as e:
@@ -463,11 +416,8 @@ def analyse_pressed_buttons_redvsblue(number):
 
 def random_leds():
     try:
-        a = random.randint(0, 3)
-        b = random.randint(0, 3)
-        while a == b:
-            b = random.randint(0, 3)
-        return [a, b]
+        return random.sample(range(4), 2)
+
     except Exception as e:
         logger.error(e)
 
@@ -478,23 +428,12 @@ def random_leds():
 def reward_response_time(response_time):
     global total_score_zen
     try:
-        print(f"response time: {response_time}")
-        if response_time < 1:
-            print("reward 10")
-            total_score_zen += 10
-        elif response_time < 3:
-            print("reward 5")
-            total_score_zen += 5
-        elif response_time < 5:
-            print("reward 3")
-            total_score_zen += 3
-        elif response_time < 10:
-            print("reward 2")
-            total_score_zen += 2
-        else:
-            print("reward 1")
-            total_score_zen += 1
-
+        rewards = [10, 5, 3, 2, 1]
+        thresholds = [1, 3, 5, 10, float('inf')]
+        reward = next(rewards[i] for i in range(
+            len(thresholds)) if response_time < thresholds[i])
+        total_score_zen += reward
+        print(f"response time: {response_time}, reward: {reward}")
         client.publish(
             "memorypoints", f"score zen:{total_score_zen} - response time:{response_time}")
     except Exception as e:
@@ -507,14 +446,13 @@ def analyse_buttons_zen(number):
     global previous_time
     global new_zen_game
     try:
-        print(f"button pressed: {number}; {game}")
         if number == random_led_zen:
             response_time = time.time() - previous_time
-            print(f"correct {response_time}")
             reward_response_time(response_time)
             new_zen_game = True
     except Exception as e:
         logger.log(e)
+
 
 # endregion
 
@@ -522,7 +460,6 @@ def analyse_buttons_zen(number):
 
 
 def analyse_buttons_minesweeper(message):
-    global game
     global index_minesweeper
     global score_minesweeper
     global haswon
@@ -530,23 +467,15 @@ def analyse_buttons_minesweeper(message):
     global busy_minesweeper
     try:
         if not busy_minesweeper:
-            print(f'button {message} pressed; game: {game}')
-            print(
-                f'controle{message == str(list_minesweeper[index_minesweeper])}')
             if message == str(list_minesweeper[index_minesweeper]):
-                print('correct')
                 client.publish(str(list_minesweeper[index_minesweeper]), "2")
                 index_minesweeper += 1
-                print(f'score: {index_minesweeper}')
                 if index_minesweeper == 4:
-                    print('game won')
                     score_minesweeper += 1
                     client.publish('memorypoints', str(score_minesweeper))
                     haswon = True
             else:
-                print('wrong')
                 if level_minesweeper == 2 or level_minesweeper == 3:
-                    print('game lost')
                     haslost = True
     except Exception as e:
         logger.error(e)
@@ -554,8 +483,6 @@ def analyse_buttons_minesweeper(message):
 
 def send_hint_function():
     global list_minesweeper
-    global index_minesweeper
-    global level_minesweeper
     global busy_minesweeper
     try:
         busy_minesweeper = True
@@ -571,7 +498,6 @@ def sequence_off():
     global busy_minesweeper
     try:
         busy_minesweeper = True
-        print('sequence off')
         for i in range(4):
             client.publish(str(i), "off")
         time.sleep(1)
@@ -584,11 +510,9 @@ def sequence_mistake():
     global busy_minesweeper
     try:
         busy_minesweeper = True
-        print('mistake')
         for i in range(4):
             client.publish(str(i), "0")
-        time.sleep(1)
-        for i in range(4):
+            time.sleep(0.2)
             client.publish(str(i), "off")
         busy_minesweeper = False
     except Exception as e:
