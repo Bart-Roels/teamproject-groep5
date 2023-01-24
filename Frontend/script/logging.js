adc2139f0387e4f7a624411480972994481bc156
Bart-Roels <bart.roels@student.howest.be>
2023-01-24 15:07:07 +0100

    Feat: add HTML

---
+++ Frontend/script/logging.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('app.log'),
+});
+
+rl.on('line', (line) => {
+  console.log(line);
+});
+
+/*document.addEventListener('DOMContentLoaded', function () {
+  var table = new DataTable('#example');
+  getData();
+});*/
