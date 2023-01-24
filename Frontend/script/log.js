b4665932cb2f2e3af61709030975f6a0358b675f
PattynRuben <ruben.pattyn@student.howest.be>
2023-01-24 14:55:33 +0100

    feat: kleine aanpassing

--- Frontend/script/log.js
+++ Frontend/script/log.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var table = new DataTable('#example');
-});
+const fs = require('fs');
+const readline = require('readline');
+
+const rl =readline.createInterface({
+    input:fs.createReadStream('app.log')
+})
+
+rl.on('line',(line)=>{
+    console.log(line);})
+
+/*document.addEventListener('DOMContentLoaded', function () {
+  var table = new DataTable('#example');
+  getData();
+});*/
