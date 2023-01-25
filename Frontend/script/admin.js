5f4ef7994142c1582cca18fb2ad175b7583b77f7
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-01-25 10:10:59 +0100

    feat: batter ready

--- Frontend/script/admin.js
+++ Frontend/script/admin.js
@@ -4,13 +4,18 @@ const client = mqtt.connect('ws://localhost:9001');
 client.on('connect', () => {
   console.log('Connected to the MQTT WebSocket');
 
-  client.subscribe('bat');
+  client.subscribe('bat1');
+  client.subscribe('bat2');
+  client.subscribe('bat3');
+  client.subscribe('bat4');
 
   client.on('message', (topic, message) => {
     console.log('Message received: ', topic, message.toString());
     const batteryProcent = parseFloat(message.toString());
-    if (topic === 'bat') {
-      const batteryElement = document.querySelector('.c-battery-level');
+    const topics = ['bat1', 'bat2', 'bat3', 'bat4'];
+    if (topics.includes(topic)) {
+      const index = topics.indexOf(topic);
+      const batteryElement = document.querySelectorAll('.c-battery-level')[index];
       batteryElement.classList.toggle('c-battery-level--low', batteryProcent <= 10);
       batteryElement.classList.toggle('c-battery-level--medium', batteryProcent < 25 && batteryProcent > 10);
       batteryElement.style.width = message.toString() + '%';
@@ -31,7 +36,10 @@ const listenToNavBtns = () => {
 const init = () => {
   listenToNavBtns();
   if (document.querySelector('.js-battery-page')) {
-    client.publish('bat', 'jens');
+    client.publish('bat1');
+    client.publish('bat2');
+    client.publish('bat3');
+    client.publish('bat4');
   }
 };
 
